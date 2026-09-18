bebba49c5e3d0516935f92619de841a36fe2add4
Pulkit Srivastava <pulkitsriv30@gmail.com>
2026-09-18 12:42:43 +0530

    Time: 7 ms (46.46%) | Memory: 20.2 MB (25.33%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,74 +1,34 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& vis,vector<int>& pathVis) {
-        for(auto it : adj[node]) {
-            if(!vis[it]) {
-                vis[it]=1;
-                pathVis[it]=1;
-                if(dfs(it,adj,vis,pathVis)==true)
-                return true;
-            }
-            else if(vis[it] && pathVis[it])
-            return true;
-        }
-        pathVis[node]=0;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses,0);
-        vector<int> pathVis(numCourses,0);
-        vector<vector<int>> adj(numCourses);
-        for(auto edge: prerequisites) {
-            int u=edge[0];
-            int v=edge[1];
+        int V=numCourses;
+        int count=0;
+        vector<int> inDegree(V,0);
+        vector<vector<int>> adj(V);
+        for(auto it  : prerequisites) {
+            int u=it[0];
+            int v=it[1];
+            inDegree[v]++;
             adj[u].push_back(v);
         }
-        for(int i=0;i<numCourses;i++) {
-            if(!vis[i]) {
-                vis[i]=1;
-                pathVis[i]=1;
-                if(dfs(i,adj,vis,pathVis))
-                return false;
+        queue<int> q;
+        for(int i=0;i<V;i++) {
+            if(inDegree[i]==0)
+            q.push(i);
+        }
+        while(!q.empty()) {
+            int node=q.front();
+            count++;
+            q.pop();
+            for(auto it : adj[node]) {
+                inDegree[it]--;
+                if(inDegree[it]==0)
+                q.push(it);
             }
         }
+        if(count==V)
         return true;
+        else 
+        return false;
     }
 };
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // int vis[numCourses]={0};
-        // vis[0]=1;
-        // queue<pair<int,int>> q;
-        // q.push({0,-1});
-        // while(!q.empty()) {
-        //     int node=q.front().first;
-        //     int parent=q.front().second;
-        //     for(auto it : adj[node]) {
-        //         if(!vis[it]) {
-        //             vis[it]=1;
-        //             q.push({it,node});
-        //         }
-        //         else if(parent!=it) {
-        //             return true;
-        //         }
-        //     }   
-        // }
-        // return false; 
